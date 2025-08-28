289f52ad5011a9c1598203e3a92761607e2ea28e
jpassgo <jeffpascoe9@gmail.com>
2025-08-28 12:07:06 -0500

    Changes made during coding interview

--- src/main/java/com/pascoe/products_api/entities/ProductEntity.java
+++ src/main/java/com/pascoe/products_api/entities/ProductEntity.java
@@ -7,7 +7,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 
-@Data
 @Entity
 @Builder
 @NoArgsConstructor
--- src/main/java/com/pascoe/products_api/repositories/ProductRepository.java
+++ src/main/java/com/pascoe/products_api/repositories/ProductRepository.java
@@ -2,9 +2,11 @@ package com.pascoe.products_api.repositories;
 
 import com.pascoe.products_api.entities.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
+    @Query("SELECT product FROM products WHERE LOWER(product.name) = LOWER(:name)")
     Optional<ProductEntity> findByName(String name);
 }
